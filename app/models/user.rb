979271fbb6ee012e82095953ddf08b651706dbd4
takkun5 <takuya0215sai@gmail.com>
2015-07-30 14:36:00 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,12 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
 
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -40,11 +45,14 @@ class User < ActiveRecord::Base
   end
 
   def want(item)
+    wants.create(item_id: item.id)
   end
 
   def unwant(item)
+    wants.find_by(item_id: item.id).destroy
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
