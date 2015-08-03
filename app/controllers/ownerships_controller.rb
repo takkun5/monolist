8f1b4d7db1291b037ecbe4e305217b6dd4c59d99
takkun5 <takuya0215sai@gmail.com>
2015-08-03 09:38:14 +0000

    lookup edit

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,7 +11,7 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-      response = {}
+      response = Amazon::Ecs.item_lookup(params[:asin], { response_group: 'Medium' })
       amazon_item       = response.items.first
       @item.title        = amazon_item.get('ItemAttributes/Title')
       @item.small_image  = amazon_item.get("SmallImage/URL")
@@ -21,27 +21,24 @@ class OwnershipsController < ApplicationController
       @item.raw_info        = amazon_item.get_hash
       @item.save!
     end
+  
       
       
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値ににHaveボタンが押された時にはの時は「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
     
-    if params[:item_id]
-      @item = Item.find(params[:item_id])
-      current_user.have(@item_id)
+    if params[:type] == "Have"
+      current_user.have(@item.id)
     end
     
-    if  params[:item_id]
-      @item = Item.find(params[:item_id])
-      current_user.want(@item_id)
+    if params[:type] == "Want"
+      current_user.want(@item.id)
     end
     
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
-    
    if params[:type] == "Have"
     @item = haves.haved 
     current_user.unhave(@item)
